2d9d2a83739d09cb48cb2442d7cff2d634249f02
Atishay <atishay2024@outlook.com>
2024-12-29 13:24:48 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,36 @@
+const path = require('path');
+
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: '@sucrase/webpack-loader',
-					options: {
-						transforms: ['jsx']
-					}
-				}
-			}
-		]
-	}
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: '@sucrase/webpack-loader',
+          options: {
+            transforms: ['jsx'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
